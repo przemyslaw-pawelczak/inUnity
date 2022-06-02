949866d5f0e6ba9ae6821f4729094a59ad2545b5
Felix Hartlieb <felix.hartlieb@a1.net>
2022-06-02 12:38:01 +0200

    Add files

--- code/example/CMakeFiles/call-simpel.dir/__/config/cfg_basic/startup.c.obj.d
+++
@@ -1,2 +0,0 @@
-example/CMakeFiles/call-simpel.dir/__/config/cfg_basic/startup.c.obj: \
- /home/felix/Desktop/inUnity-proto/code/config/cfg_basic/startup.c
--- code/example/CMakeFiles/call-simpel.dir/call-simple.c.obj.d
+++
@@ -1,2 +0,0 @@
-example/CMakeFiles/call-simpel.dir/call-simple.c.obj: \
- /home/felix/Desktop/inUnity-proto/code/example/call-simple.c
